829972d8a19b31d56fbb6e8dc4470ff767d81b2e
Grzegorz Kawka-Osik <mivalsten@gmail.com>
2019-08-14 19:39:35 +0000

    minor fixes

--- www/post/users.php
+++ www/post/users.php
@@ -17,11 +17,13 @@ if($_SESSION["isAdmin"] == 1 || $vars['username'] == $_SESSION["username"]) {
 				UPDATE t_users
 				SET username = '" . $vars['newUsername'] . "'
 				WHERE username = '" . $vars['username'] . "';";
+				break;
 		case 'password':
 			$sql = "
 				UPDATE t_users
 				SET passwd = '" . password_hash($vars['newPassword'], PASSWORD_BCRYPT) . "'
 				WHERE username = '" . $vars['username'] . "';";
+				break;
 		case 'type':
 			if ($_SESSION["isAdmin"] == 1) {
 				$sql = "
@@ -29,12 +31,14 @@ if($_SESSION["isAdmin"] == 1 || $vars['username'] == $_SESSION["username"]) {
 					SET isAdmin = (isAdmin + 1) % 2
 					WHERE username = '" . $vars['username'] . "';";
 			}
+			break;
 		case 'delete':
 			if ($_SESSION["isAdmin"] == 1) {
 				$sql = "
 					DELETE FROM t_users
 					WHERE username = '" . $vars['username'] . "';";
 			}
+			break;
 		case 'new':
 			$sql = "
 				INSERT INTO t_users VALUES ('" . $vars['username'] . "', '" . password_hash($vars['password'], PASSWORD_BCRYPT) . "', 0);";
